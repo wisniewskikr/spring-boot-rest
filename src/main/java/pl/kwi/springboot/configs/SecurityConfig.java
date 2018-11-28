6cb44527caf95b35715ad1d65a1e2f828324ea42
Wisniewski <wisniewk@emea.roche.com>
2018-11-28 15:22:10 +0100

    Init filter

--- src/main/java/pl/kwi/springboot/configs/SecurityConfig.java
+++ src/main/java/pl/kwi/springboot/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import pl.kwi.springboot.filters.CustomFilter;
@@ -22,9 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	http.authorizeRequests()
     		.antMatchers("/**").hasRole("USER")
         .and()
-        	.httpBasic()
+        	.x509()
         .and()
-        	.addFilterBefore(filter, BasicAuthenticationFilter.class)
+        	.addFilterBefore(filter, X509AuthenticationFilter.class)
         	.csrf().disable();
     	
     }
--- src/main/java/pl/kwi/springboot/filters/CustomFilter.java
+++ src/main/java/pl/kwi/springboot/filters/CustomFilter.java
@@ -30,52 +30,10 @@ public class CustomFilter extends OncePerRequestFilter {
             HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
     	
-    	String header = request.getHeader("Authorization");
-    	if(header == null) {
-    		filterChain.doFilter(request, response);
-    		return;
-    	}
-    	
-    	String[] tokens = extractAndDecodeHeader(header, request);
-    	assert tokens.length == 2;
-    	
-    	String username = tokens[0];
-    	String password = tokens[1];
-    	
-    	if (USERNAME.equals(username) && PASSWORD.equals(password)) {
-        	List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
-            grantedAuths.add(new SimpleGrantedAuthority(ROLE));
-            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, password, grantedAuths);
-            SecurityContextHolder.getContext().setAuthentication(auth);
-            filterChain.doFilter(request, response);
-        } else {
-            throw new
-              BadCredentialsException("External system authentication failed");
-        }
+    	filterChain.doFilter(request, response);
         
     }
     
-    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
-			throws IOException {
 
-		byte[] base64Token = header.substring(6).getBytes("UTF-8");
-		byte[] decoded;
-		try {
-			decoded = Base64.decode(base64Token);
-		}
-		catch (IllegalArgumentException e) {
-			throw new BadCredentialsException(
-					"Failed to decode basic authentication token");
-		}
-
-		String token = new String(decoded, "UTF-8");
-
-		int delim = token.indexOf(":");
-
-		if (delim == -1) {
-			throw new BadCredentialsException("Invalid basic authentication token");
-		}
-		return new String[] { token.substring(0, delim), token.substring(delim + 1) };
-	}
 
 }
